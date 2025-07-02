38c1ef0bab65bb7dfa488243ec25c5439de75a75
Dickson Okungbeni <diktech1@gmail.com>
2025-07-02 21:32:31 +0100

    Day 3: Add simple login validator

---
+++ login_validator.py
@@ -0,0 +1,16 @@
+#correct username and password
+correct_username = "admin"
+correct_password = "1234"
+
+#get user input
+username = input("Enter your username:")
+password = input("Enter your password:")
+if username == correct_username and password == correct_password:
+    print("access granted")
+else:
+    print("access denied")
+    
+    
+    
+    
+    
